b8d2edc17106385c0c5fd811b47a6df15e6d1f65
sripathman <nsripathman@gmail.com>
2023-07-26 13:15:03 +0100

    features webpack

---
+++ src/generateJoke.js
@@ -0,0 +1,6 @@
+
+function generateJoke(){
+  return "I do'nt trust stairs , They are always up to something "
+
+}
+export default (generateJoke())
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import generateJoke from "./generateJoke"
+import './styles/main.scss'
+import laughing from './assets/laughing.svg'
+const laughImg = document.getElementById('laughImg')
+laughImg.src = laughing
+console.log(generateJoke())
